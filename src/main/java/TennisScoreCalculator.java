cdc57364207cc0365219deef8c3c3e6345c33c33
Tom <thomas.spencer@codurance.com>
2020-11-19 08:06:30 +0000

    Thirty Love - strawberries and cream!

--- src/main/java/TennisScoreCalculator.java
+++ src/main/java/TennisScoreCalculator.java
@@ -1,5 +1,14 @@
+import java.util.Map;
+
 public class TennisScoreCalculator {
-    public String score(int player1Points, int player2Points) {
-        throw new UnsupportedOperationException("Implement me!");
-    }
+
+  private final Map<Integer, String> scores = Map.of(
+      0, "love",
+      1, "fifteen",
+      2, "thirty"
+  );
+
+  public String score(int player1Points, int player2Points) {
+    return scores.get(player1Points) + " " + scores.get(player2Points);
+  }
 }
--- src/test/java/TennisScoreCalculatorTest.java
+++ src/test/java/TennisScoreCalculatorTest.java
@@ -1,12 +1,37 @@
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+// Test list
+// zombies - zero, one, many, boundary, interface, exceptional conditions, simple test - simple solutions
+// 0 0
+// 1 0
+// 0 1
+// 1 1
+// 2 0
+// 2 1
+// 2 2
+// 3 0
+// 3 1
+// 3 2
+// 3 3 - deuce!
+// 4 3 - advantage player 1!
+// 4 4 - deuce!
+// 4 5 - advantage player 2!
+// 4 6 - Game player 1!
+// You cannot be serious!
 class TennisScoreCalculatorTest {
-    @Test
-    void return_love_love_for_0_0() {
-        TennisScoreCalculator tennisScoreCalculator = new TennisScoreCalculator();
-
-        assertEquals(tennisScoreCalculator.score(0, 0), "Love love");
-    }
+  @ParameterizedTest
+  @CsvSource({
+      "0, 0, 'love love'",
+      "1, 0, 'fifteen love'",
+      "0, 1, 'love fifteen'",
+      "1, 1, 'fifteen fifteen'",
+      "2, 0, 'thirty love'"
+  })
+  void return_love_love_for_0_0(int player1Points, int player2Points, String result) {
+    TennisScoreCalculator tennisScoreCalculator = new TennisScoreCalculator();
+    assertEquals(tennisScoreCalculator.score(player1Points, player2Points), result);
+  }
 }
